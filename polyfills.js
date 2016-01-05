dbfbe583b257ac2c1d472bb29eb1989008fffaf3
Artem Artemyev <aartemev@naumen.ru>
2016-01-06 02:39:11 +0500

    added a Map to polyfills

--- polyfills.js
+++ polyfills.js
@@ -2,8 +2,10 @@ if (typeof window !== 'undefined') {
   window.Symbol = require("core-js/es6/symbol");
   window.Promise = require("core-js/es6/promise");
   window.Set = require("core-js/es6/set");
+  window.Map = require("core-js/es6/map");
 } else {
   global.Symbol = require("core-js/es6/symbol");
   global.Promise = require("core-js/es6/promise");
   global.Set = require("core-js/es6/set");
+  global.Map = require("core-js/es6/map");
 }
